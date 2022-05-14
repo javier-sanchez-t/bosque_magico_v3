49653d6b741f1c22b0f2bd92c1b6c6302d2798f0
Javier Sanchez <jefe10jav@gmail.com>
2022-05-14 17:53:18 -0500

    JST - Nercado

--- js/mercadoV2.js
+++ js/mercadoV2.js
@@ -1,29 +1,49 @@
 window.onload = function () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const option = urlParams.get('option')
+  const option = urlParams.get("option");
   var section = "";
   var btnOption = "";
 
   switch (option) {
-    case 'MERCADO':
-      section = document.getElementById("MERCADO");
-      btnOption = document.getElementById("BTN_MERCADO");
+    case "HAMBURGUESAS":
+      section = document.getElementById("HAMBURGUESAS");
+      btnOption = document.getElementById("BTN_HAMBURGUESAS");
       break;
-    case 'MOKA':
-      section = document.getElementById("MOKA");
-      btnOption = document.getElementById("BTN_MOKA");
+    case "COMIDA_REGIONAL":
+      section = document.getElementById("COMIDA_REGIONAL");
+      btnOption = document.getElementById("BTN_COMIDA_REGIONAL");
       break;
-    case 'CHURRERIA':
-      section = document.getElementById("CHURRERIA");
-      btnOption = document.getElementById("BTN_CHURRERIA");
+    case "RECUERDOS":
+      section = document.getElementById("RECUERDOS");
+      btnOption = document.getElementById("BTN_RECUERDOS");
+      break;
+    case "BARBACOA":
+      section = document.getElementById("BARBACOA");
+      btnOption = document.getElementById("BTN_BARBACOA");
+      break;
+    case "TACO_ASADA":
+      section = document.getElementById("TACO_ASADA");
+      btnOption = document.getElementById("BTN_TACO_ASADA");
+      break;
+    case "WAFFLES":
+      section = document.getElementById("WAFFLES");
+      btnOption = document.getElementById("BTN_WAFFLES");
+      break;
+    case "CREPAS":
+      section = document.getElementById("CREPAS");
+      btnOption = document.getElementById("BTN_CREPAS");
+      break;
+    case "LA_ANTIGUA":
+      section = document.getElementById("LA_ANTIGUA");
+      btnOption = document.getElementById("BTN_LA_ANTIGUA");
       break;
     default:
-      section = document.getElementById("MOKA");
-      btnOption = document.getElementById("BTN_MOKA");
+      section = document.getElementById("LA_ANTIGUA");
+      btnOption = document.getElementById("BTN_LA_ANTIGUA");
   }
 
-  btnOption.classList.add("shadow");
+  btnOption.firstElementChild.classList.add("shadow");
   section.classList.add("show");
-  window.location.href = "#OPTIONS";
+  window.location.href = "#BTN_HAMBURGUESAS";
 };
