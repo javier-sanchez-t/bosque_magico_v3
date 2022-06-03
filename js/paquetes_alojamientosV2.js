6abbdc6c57997b45bb95f036b8370c51fccafa04
Javier Sanchez <jefe10jav@gmail.com>
2022-06-02 19:53:41 -0500

    JST - slider

--- js/paquetes_alojamientosV2.js
+++ js/paquetes_alojamientosV2.js
@@ -1,40 +1,52 @@
 window.onload = function () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const option = urlParams.get('option')
+  const option = urlParams.get("option");
   var section = "";
   var btnOption = "";
+  var sliderWrapper = document.getElementsByClassName("swiper-wrapper")[0];
+  var images = [
+    "./img/recorridos/Luciernagas/bosque-magico-luciernagas-001.jpg",
+    "./img/luciernagas/bosque-magico-luciernagas-002.jpg",
+    "./img/luciernagas/bosque-magico-luciernagas-005.jpg",
+    "./img/luciernagas/bosque-magico-luciernagas-006.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-6.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-2.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-4.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-8.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-7.jpg",
+  ];
 
   switch (option) {
-    case 'PAQUETE_BRONCE_LA_TORRE':
+    case "PAQUETE_BRONCE_LA_TORRE":
       section = document.getElementById("PAQUETE_BRONCE_LA_TORRE");
       btnOption = document.getElementById("BTN_PAQUETE_BRONCE_LA_TORRE");
       break;
-    case 'PAQUETE_BRONCE_HOTEL_ALBA':
+    case "PAQUETE_BRONCE_HOTEL_ALBA":
       section = document.getElementById("PAQUETE_BRONCE_HOTEL_ALBA");
       btnOption = document.getElementById("BTN_PAQUETE_BRONCE_HOTEL_ALBA");
       break;
-    case 'PAQUETE_BRONCE_LOS_PINOS':
+    case "PAQUETE_BRONCE_LOS_PINOS":
       section = document.getElementById("PAQUETE_BRONCE_LOS_PINOS");
       btnOption = document.getElementById("BTN_PAQUETE_BRONCE_LOS_PINOS");
       break;
-    case 'PAQUETE_FINCA_ZAYAS':
+    case "PAQUETE_FINCA_ZAYAS":
       section = document.getElementById("PAQUETE_FINCA_ZAYAS");
       btnOption = document.getElementById("BTN_PAQUETE_FINCA_ZAYAS");
       break;
-    case 'PAQUETE_PLATINO_SAN_CAYETANO':
+    case "PAQUETE_PLATINO_SAN_CAYETANO":
       section = document.getElementById("PAQUETE_PLATINO_SAN_CAYETANO");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_SAN_CAYETANO");
       break;
-    case 'PAQUETE_PLATINO_VILLAS_BOSQUE':
+    case "PAQUETE_PLATINO_VILLAS_BOSQUE":
       section = document.getElementById("PAQUETE_PLATINO_VILLAS_BOSQUE");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_VILLAS_BOSQUE");
       break;
-    case 'PAQUETE_PIEDRA_CANTEADA':
+    case "PAQUETE_PIEDRA_CANTEADA":
       section = document.getElementById("PAQUETE_PIEDRA_CANTEADA");
       btnOption = document.getElementById("BTN_PAQUETE_PIEDRA_CANTEADA");
       break;
-    case 'PAQUETE_PLATINO_SIERRA_NEVADA':
+    case "PAQUETE_PLATINO_SIERRA_NEVADA":
       section = document.getElementById("PAQUETE_PLATINO_SIERRA_NEVADA");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_SIERRA_NEVADA");
       break;
@@ -46,4 +58,45 @@ window.onload = function () {
   btnOption.firstElementChild.classList.add("shadow");
   section.classList.add("show");
   window.location.href = "#OPTIONS";
+
+  if (sliderWrapper) {
+    images.forEach((image) => {
+      var slide = document.createElement("div");
+      slide.style.backgroundImage = "url('" + image + "')";
+      slide.classList.add("swiper-slide");
+      slide.classList.add("custom-slide");
+
+      console.log(slide.style);
+      sliderWrapper.appendChild(slide);
+    });
+  }
+
+  // NEW SLIDER
+  const slider = document.getElementsByClassName("swiper")[0];
+  if (window.innerWidth > 600 && slider) {
+    const prevBtn = document.createElement("div");
+    prevBtn.classList.add("swiper-button-prev");
+    slider.appendChild(prevBtn);
+
+    const nextBtn = document.createElement("div");
+    nextBtn.classList.add("swiper-button-next");
+    slider.appendChild(nextBtn);
+  }
+
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    //loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 };
--- js/paquetes_rutasV2.js
+++ js/paquetes_rutasV2.js
@@ -1,28 +1,40 @@
 window.onload = function () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const option = urlParams.get('option')
+  const option = urlParams.get("option");
   var section = "";
   var btnOption = "";
+  var sliderWrapper = document.getElementsByClassName("swiper-wrapper")[0];
+  var images = [
+    "./img/recorridos/Luciernagas/bosque-magico-luciernagas-001.jpg",
+    "./img/luciernagas/bosque-magico-luciernagas-002.jpg",
+    "./img/luciernagas/bosque-magico-luciernagas-005.jpg",
+    "./img/luciernagas/bosque-magico-luciernagas-006.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-6.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-2.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-4.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-8.jpg",
+    "./img/recorridos/Magueyes/bosque-magico-santuario-de-las-luciernegas-A001-7.jpg",
+  ];
 
   switch (option) {
-    case 'PENA_TIGRE':
+    case "PENA_TIGRE":
       section = document.getElementById("PENA_TIGRE");
       btnOption = document.getElementById("BTN_PENA_TIGRE");
       break;
-    case 'MONUMENTOS_HACIENDAS':
+    case "MONUMENTOS_HACIENDAS":
       section = document.getElementById("MONUMENTOS_HACIENDAS");
       btnOption = document.getElementById("BTN_MONUMENTOS_HACIENDAS");
       break;
-    case 'HACIENDA_MEXICANA':
+    case "HACIENDA_MEXICANA":
       section = document.getElementById("HACIENDA_MEXICANA");
       btnOption = document.getElementById("BTN_HACIENDA_MEXICANA");
       break;
-    case 'MAGUEYES_PULQUE':
+    case "MAGUEYES_PULQUE":
       section = document.getElementById("MAGUEYES_PULQUE");
       btnOption = document.getElementById("BTN_MAGUEYES_PULQUE");
       break;
-    case 'BOSQUE_LUCIERNAGAS':
+    case "BOSQUE_LUCIERNAGAS":
       section = document.getElementById("BOSQUE_LUCIERNAGAS");
       btnOption = document.getElementById("BTN_BOSQUE_LUCIERNAGAS");
       break;
@@ -34,4 +46,45 @@ window.onload = function () {
   btnOption.firstElementChild.classList.add("shadow");
   section.classList.add("show");
   window.location.href = "#OPTIONS";
+
+  if (sliderWrapper) {
+    images.forEach((image) => {
+      var slide = document.createElement("div");
+      slide.style.backgroundImage = "url('" + image + "')";
+      slide.classList.add("swiper-slide");
+      slide.classList.add("custom-slide");
+
+      console.log(slide.style);
+      sliderWrapper.appendChild(slide);
+    });
+  }
+
+  // NEW SLIDER
+  const slider = document.getElementsByClassName("swiper")[0];
+  if (window.innerWidth > 600 && slider) {
+    const prevBtn = document.createElement("div");
+    prevBtn.classList.add("swiper-button-prev");
+    slider.appendChild(prevBtn);
+
+    const nextBtn = document.createElement("div");
+    nextBtn.classList.add("swiper-button-next");
+    slider.appendChild(nextBtn);
+  }
+
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    //loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 };
