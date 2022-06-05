ac41ded629a116c208347bac84c77a981806343f
javier-sanchez-t <jefe10jav@gmail.com>
2022-06-05 16:56:26 -0500

    JST - paquetes

--- js_v2/paquetes_alojamientosV2.js
+++ js_v2/paquetes_alojamientosV2.js
@@ -5,121 +5,61 @@ window.onload = function () {
   var section = "";
   var btnOption = "";
   var sliderWrapper = document.getElementsByClassName("swiper-wrapper")[0];
-  var images = [];
+  var images = [
+    "./img/paquetes/circuito_tranvia_bosque_magico.jpg",
+    "./img/paquetes/ruta_del_pulque_nanacamilpa.jpg",
+    "./img/paquetes/santuario_luciernagas_tlaxcala.jpg",
+  ];
 
   switch (option) {
     case "PAQUETE_BRONCE_LA_TORRE":
       section = document.getElementById("PAQUETE_BRONCE_LA_TORRE");
       btnOption = document.getElementById("BTN_PAQUETE_BRONCE_LA_TORRE");
-      images = [
-        "./img/alojamientos/Auto_hotel_la_torre/rancho_la_torre.jpg",
-      ];
+      images.push("./img/alojamientos/Auto_hotel_la_torre/rancho_la_torre.jpg");
       break;
     case "PAQUETE_BRONCE_HOTEL_ALBA":
       section = document.getElementById("PAQUETE_BRONCE_HOTEL_ALBA");
       btnOption = document.getElementById("BTN_PAQUETE_BRONCE_HOTEL_ALBA");
-      images = [
-        "./img/alojamientos/Hotel_alba/doble_hotel_alba.jpg",
-        "./img/alojamientos/Hotel_alba/estancia_hotel_alba.jpg",
-        "./img/alojamientos/Hotel_alba/fachada_hotel_alba.jpg",
-        "./img/alojamientos/Hotel_alba/habitacion_sencilla_hotel_alba.jpg",
-        "./img/alojamientos/Hotel_alba/habitacion_tres_personas_hotel_alba.jpg",
-        "./img/alojamientos/Hotel_alba/habitacion_triple_hotel_alba.jpg",
-        "./img/alojamientos/Hotel_alba/litera_hotel_alba.jpg",
-      ];
+      images.push("./img/paquetes/alojamientos/hotel_alba/fachada_hotel_alba.jpg");
+      images.push("./img/paquetes/alojamientos/hotel_alba/habitacion_sencilla_hotel_alba.jpg");
       break;
     case "PAQUETE_BRONCE_LOS_PINOS":
       section = document.getElementById("PAQUETE_BRONCE_LOS_PINOS");
       btnOption = document.getElementById("BTN_PAQUETE_BRONCE_LOS_PINOS");
-      images = [
-        "./img/alojamientos/Posada_los_pinos/cocina_los_pinos.jpg",
-        "./img/alojamientos/Posada_los_pinos/comedor_posada_los_pinos.jpg",
-        "./img/alojamientos/Posada_los_pinos/estancia_posada_los_pinos.jpg",
-        "./img/alojamientos/Posada_los_pinos/habitacion_adicional_posada_los_pinos.jpg",
-        "./img/alojamientos/Posada_los_pinos/habitacion_posada_los_pinos.jpg",
-        "./img/alojamientos/Posada_los_pinos/habitacion_sencilla_posada_los_pinos.jpg",
-        "./img/alojamientos/Posada_los_pinos/recamara_posada_los_pinos.jpg",
-      ];
+      images.push("./img/paquetes/alojamientos/los_pinos/habitacion_adicional_posada_los_pinos.jpg");
+      images.push("./img/paquetes/alojamientos/los_pinos/habitacion_sencilla_posada_los_pinos.jpg");
       break;
     case "PAQUETE_FINCA_ZAYAS":
       section = document.getElementById("PAQUETE_FINCA_ZAYAS");
       btnOption = document.getElementById("BTN_PAQUETE_FINCA_ZAYAS");
-      images = [
-        "./img/alojamientos/La_Finca_Zayas/acceso_salon_finca_zayas.jpg",
-        "./img/alojamientos/La_Finca_Zayas/capilla_finca_zayas.jpg",
-        "./img/alojamientos/La_Finca_Zayas/finca_zayas.jpg",
-        "./img/alojamientos/La_Finca_Zayas/habitacion_finca_zayas.jpg",
-        "./img/alojamientos/La_Finca_Zayas/habitaciones_la_finca_zayas.jpg",
-        "./img/alojamientos/La_Finca_Zayas/hospedaje_finca_zayas.jpg",
-        "./img/alojamientos/La_Finca_Zayas/lago_finca_zayas.jpg",
-      ];
+      images.push("./img/paquetes/alojamientos/finca_zayas/finca_zayas.jpg");
+      images.push("./img/paquetes/alojamientos/finca_zayas/habitaciones_la_finca_zayas.jpg");
+      images.push("./img/paquetes/alojamientos/finca_zayas/hospedaje_finca_zayas.jpg");
       break;
     case "PAQUETE_PLATINO_SAN_CAYETANO":
       section = document.getElementById("PAQUETE_PLATINO_SAN_CAYETANO");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_SAN_CAYETANO");
-      images = [
-        "./img/alojamientos/Hacienda_san_cayetano/estancia_hacienda_san_cayetano.jpg",
-        "./img/alojamientos/Hacienda_san_cayetano/acceso_hacienda_san_cayetano.jpg",
-        "./img/alojamientos/Hacienda_san_cayetano/bar_hacienda_san_cayetano.jpg",
-        "./img/alojamientos/Hacienda_san_cayetano/habitacion_hacienda_san_cayetano.jpg",
-        "./img/alojamientos/Hacienda_san_cayetano/patio_interior_hacienda_san_cayetano.jpg",
-        "./img/alojamientos/Hacienda_san_cayetano/pasillo_exterior_hacienda_san_cayetano.jpg",
-        "./img/alojamientos/Hacienda_san_cayetano/pasillo_interior_hacienda_san_cayetano.jpg",
-      ];
+      images.push("./img/paquetes/alojamientos/san_cayetano/habitacion_hacienda_san_cayetano.jpg");
       break;
     case "PAQUETE_PLATINO_VILLAS_BOSQUE":
       section = document.getElementById("PAQUETE_PLATINO_VILLAS_BOSQUE");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_VILLAS_BOSQUE");
-      images = [
-        "./img/alojamientos/Villas_del_bosque/asador_cabana_villas_del_bosque.jpg",
-        "./img/alojamientos/Villas_del_bosque/cabana_familiar.jpg",
-        "./img/alojamientos/Villas_del_bosque/cabana_parejas.jpg",
-        "./img/alojamientos/Villas_del_bosque/cabana_santa_clara.jpg",
-        "./img/alojamientos/Villas_del_bosque/cabana_villas_del_bosque.jpg",
-        "./img/alojamientos/Villas_del_bosque/cabanas_tlaxcala.jpg",
-        "./img/alojamientos/Villas_del_bosque/sala_cabana_santa_clara.jpg",
-      ];
+      images.push("./img/paquetes/alojamientos/villas_del_bosque/cabana_villas_del_bosque.jpg");
+      images.push("./img/paquetes/alojamientos/villas_del_bosque/sala_cabana_santa_clara.jpg");
       break;
     case "PAQUETE_PIEDRA_CANTEADA":
       section = document.getElementById("PAQUETE_PIEDRA_CANTEADA");
       btnOption = document.getElementById("BTN_PAQUETE_PIEDRA_CANTEADA");
-      images = [
-        "./img/alojamientos/Piedra_canteada/cabana_doble_piedra_canteada.jpg",
-        "./img/alojamientos/Piedra_canteada/cabana_sencilla_piedra_canteada.jpg",
-        "./img/alojamientos/Piedra_canteada/cabana_triple_piedra_canteada.jpg",
-        "./img/alojamientos/Piedra_canteada/cabanas_piedra_canteada.jpg",
-        "./img/alojamientos/Piedra_canteada/camino_piedra_canteada.jpg",
-        "./img/alojamientos/Piedra_canteada/eco_hotel_piedra_canteada.jpg",
-        "./img/alojamientos/Piedra_canteada/acceso_piedra_canteada.jpg",
-      ];
+      images.push("./img/paquetes/alojamientos/piedra_canteada/cabana_doble_piedra_canteada.jpg");
+      images.push("./img/paquetes/alojamientos/piedra_canteada/camino_piedra_canteada.jpg");
       break;
     case "PAQUETE_PLATINO_SIERRA_NEVADA":
       section = document.getElementById("PAQUETE_PLATINO_SIERRA_NEVADA");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_SIERRA_NEVADA");
-      images = [
-        "./img/alojamientos/Villas bosque/cabana_dos_parejas.png",
-        "./img/alojamientos/Villas bosque/recamara_cabana.png",
-        "./img/alojamientos/Villas bosque/sala_cabana.png",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-10.jpg",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-8.jpg",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-5.jpg",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-4.jpg",
-        "./img/alojamientos/Hotel Alba/bosque-magico-santuario-de-las-luciernegas-A001-2.jpg"
-      ];
       break;
     default:
       section = document.getElementById("PAQUETE_PLATINO_SIERRA_NEVADA");
       btnOption = document.getElementById("BTN_PAQUETE_PLATINO_SIERRA_NEVADA");
-      images = [
-        "./img/alojamientos/Villas bosque/cabana_dos_parejas.png",
-        "./img/alojamientos/Villas bosque/recamara_cabana.png",
-        "./img/alojamientos/Villas bosque/sala_cabana.png",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-10.jpg",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-8.jpg",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-5.jpg",
-        "./img/alojamientos/Posada los Pinos/bosque-magico-santuario-de-las-luciernegas-A001-4.jpg",
-        "./img/alojamientos/Hotel Alba/bosque-magico-santuario-de-las-luciernegas-A001-2.jpg"
-      ];
   }
 
   btnOption.firstElementChild.classList.add("shadow");
